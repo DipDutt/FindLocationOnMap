1d340f11dce2a69ac8fb6a6d0d99355c04a17195
Dip Dutt <emduttt456@gmail.com>
2024-11-20 17:21:58 +0600

    Initial Commit

---
+++ FindLocationOnMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FindLocationOnMap
+//
+//  Created by Dip Dutt on 20/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FindLocationOnMap/FindLocationOnMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindLocationOnMapApp.swift
+//  FindLocationOnMap
+//
+//  Created by Dip Dutt on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FindLocationOnMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
